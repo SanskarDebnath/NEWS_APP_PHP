0b891ed32e98badb4abde996b23467409bc09308
SanskarDebnath <sanskardebnath2019@gmail.com>
2023-09-15 16:48:45 +0530

    just created

---
+++ admin/addnews.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin page</title>
+    <link rel="stylesheet" href="adminstyle.css">
+</head>
+<body>
+    <form action="adminserver.php?news=1" method="POST" class="add_news" id="add_news">
+        
+    </form>
+</body>
+<script src="adminscripts.js"></script>
+</html>
