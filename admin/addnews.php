8da8a8581b8d6477acdd6837c569c8655a917f7f
SanskarDebnath <sanskardebnath2019@gmail.com>
2023-09-15 16:59:35 +0530

    Update addnews.php

--- admin/addnews.php
+++ admin/addnews.php
@@ -8,7 +8,16 @@
 </head>
 <body>
     <form action="adminserver.php?news=1" method="POST" class="add_news" id="add_news">
-        
+        <div class="About">
+            <label>Write the Headline of the News</label>
+            <input type="text" name="news_title" id= "news_name"><br>
+
+            <label>Write the full description about the News</label>
+            <textarea name="About_news" id ="about_news" placeholder="Insert the details of the news here"></textarea><br>
+
+            <label>Select the Date of the News</label>
+            <input type="Date" name="news_date">
+        </div>
     </form>
 </body>
 <script src="adminscripts.js"></script>
