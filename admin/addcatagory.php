6a0e29c1f4a3725adea922630fe46a96233a08ba
SanskarDebnath <sanskardebnath2019@gmail.com>
2023-09-15 18:39:59 +0530

    Update addcatagory.php

--- admin/addcatagory.php
+++ admin/addcatagory.php
@@ -1,20 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f1f1f1;
+            margin: 0;
+            padding: 0;
+        }
+
+        .category {
+            max-width: 400px;
+            margin: 0 auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0px 0px 10px #d3d3d3;
+        }
+
+        .category label {
+            display: block;
+            margin-bottom: 10px;
+            font-weight: bold;
+        }
+
+        .category input[type="number"],
+        .category input[type="text"],
+        .category input[type="submit"] {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 15px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+
+        .category input[type="submit"] {
+            background-color: #4CAF50;
+            color: #fff;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        .category input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+    </style>
 </head>
+
 <body>
-<div class="category">
-    <form method="POST" action="adminserver.php?news=1" id="method_form">
-        <label>Select the Category ID</label>
-        <input type="number" name="CIN" id="CIN">
-        <br>
-        <input type="text" id="catagory" placeholder="Enter the Category Name"><br>
-        <input type="submit" name="CINsubmit" id="CINsubmit" value="SUBMIT">
-    </form>
-</div>
+    <div class="category">
+        <form method="POST" action="adminserver.php?news=1" id="method_form">
+            <label>Select the Category ID</label>
+            <input type="number" name="CIN" id="CIN">
+            <br>
+            <input type="text" id="catagory" placeholder="Enter the Category Name"><br>
+            <input type="submit" name="CINsubmit" id="CINsubmit" value="SUBMIT">
+        </form>
+    </div>
 </body>
 
 <script>
