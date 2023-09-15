fb2a2ac37b47f1f4a5420ce84034654d9b953432
SanskarDebnath <sanskardebnath2019@gmail.com>
2023-09-15 20:36:43 +0530

    Update adminscripts.js

--- admin/adminscripts.js
+++ admin/adminscripts.js
@@ -1,13 +1,13 @@
 const newsDateInput = document.getElementById("news_date");
-const today = new Date().toISOString().split("T")[0]; // Get today's date in ISO format
+const today = new Date().toISOString().split("T")[0];
 newsDateInput.setAttribute("max", today);
 
 
 function checkFileSize(input) {
-    const file = input.files[0]; // Get the selected file
+    const file = input.files[0];
     if (!file) {
         alert("Please select a file.");
-        input.value = ''; // Clear the input
+        input.value = '';
         return;s
     }
 
@@ -15,6 +15,44 @@ function checkFileSize(input) {
 
     if (file.size > maxSizeInBytes) {
         alert("File size exceeds the maximum allowed size (2MB). Please select a smaller file.");
-        input.value = ''; // Clear the input
+        input.value = '';
     }
 }
+document.addEventListener("DOMContentLoaded", function() {
+    var newsNameInput = document.getElementById("news_name");
+    var aboutNewsInput = document.getElementById("about_news");
+    var newsDateInput = document.getElementById("news_date");
+
+    newsNameInput.addEventListener("input", function() {
+        var value = newsNameInput.value;
+        if (value.length > 70) {
+            alert("News headline can't exceed 70 characters. Excess characters will be trimmed.");
+            newsNameInput.value = value.slice(0, 70);
+        }
+    });
+
+    aboutNewsInput.addEventListener("input", function() {
+        var value = aboutNewsInput.value;
+        if (value.length > 800) {
+            alert("News description can't exceed 600 characters. Excess characters will be trimmed.");
+            aboutNewsInput.value = value.slice(0, 800);
+        }
+    });
+
+    document.getElementById("add_news").addEventListener("submit", function(event) {
+        if (newsNameInput.value.trim() === "") {
+            alert("News headline can't be empty.");
+            event.preventDefault();
+        }
+
+        if (aboutNewsInput.value.trim() === "") {
+            alert("News description can't be empty.");
+            event.preventDefault();
+        }
+
+        if (newsDateInput.value.trim() === "") {
+            alert("Please select a date for the news.");
+            event.preventDefault();
+        }
+    });
+});
