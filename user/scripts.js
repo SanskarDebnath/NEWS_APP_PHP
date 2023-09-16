52d4a44d7873f96758576225a6d444de8c1b620d
SanskarDebnath <sanskardebnath2019@gmail.com>
2023-09-16 22:08:18 +0530

    gg

--- user/scripts.js
+++ user/scripts.js
@@ -17,3 +17,28 @@ document.querySelectorAll(".view-news-btn").forEach(function (button, index) {
         currentOffcanvas = target;
     });
 });
+
+const fullScreenButton = document.getElementById("full_screen");
+
+fullScreenButton.addEventListener("click", () => {
+    if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen().catch(err => {
+            console.error('Error attempting to enable full-screen mode:', err);
+        });
+    } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+        document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
+        document.documentElement.webkitRequestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+        document.documentElement.msRequestFullscreen();
+    }
+});
+
+const darkModeToggle = document.getElementById("dark_mode_toggle");
+const body = document.body;
+
+darkModeToggle.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+});
+
+
