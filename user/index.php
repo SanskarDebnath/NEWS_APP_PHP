bc7080bc5a1ac82e695cb56be6d9c9376496734d
SanskarDebnath <sanskardebnath2019@gmail.com>
2023-09-19 11:58:30 +0530

    Update index.php

--- user/index.php
+++ user/index.php
@@ -354,7 +354,22 @@ $currentDate = getCurrentDate();
         }
         ?>
     </div>
+</div><br><br>
+
+<!-- 
+<div id="popuilar">
+<div class="row g-0 bg-body-secondary position-relative">
+  <div class="col-md-6 mb-md-0 p-md-4">
+    <img src="..." class="w-100" alt="...">
+  </div>
+  <div class="col-md-6 p-4 ps-md-0">
+    <h5 class="mt-0">Columns with stretched link</h5>
+    <p>Another instance of placeholder content for this other custom component. It is intended to mimic what some real-world content would look like, and we're using it here to give the component a bit of body and size.</p>
+    <a href="#" class="stretched-link">Go somewhere</a>
+  </div>
 </div>
+</div> -->
+
     <script src="scripts.js"></script>
 </body>
 </html>
